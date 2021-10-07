fd0a6ff054794dd8d913db7eb269bd2b86685585
ponraj <ponrajmoorthi1999@gmail.com>
2021-10-07 10:37:42 +0530

    codeupdated

--- src/org/recall/in/Navigation.java
+++ src/org/recall/in/Navigation.java
@@ -4,11 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Navigation {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","E:\\eclipse-committers-oxygen-3a-win32-x86_64\\Program Files\\Selenium\\driver\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.google.com/");
 		driver.manage().window().maximize();
+		Thread.sleep(3000);
 		driver.navigate().to("https://www.facebook.com/");
 		driver.navigate().back();
 		driver.navigate().forward();
